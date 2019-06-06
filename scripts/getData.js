b255e0d3061db3ef9f2aa5ecbbc795df99ca738e
Nate Fischer <ntfschr@google.com>
2019-06-06 00:17:51 -0700

    chore: bump eslint version
    This removes a dependency on eslint (the previously-depended on version
    has a security vulnerability).
    
    This also resolves lint errors raised by the new version.

--- scripts/getData.js
+++ scripts/getData.js
@@ -4,14 +4,14 @@ var fs = require('fs');
 gsjson({
   spreadsheetId: '1h6dVJKtETWX3Kr9PT6EaLu0gGavdi8Gnj4IlX155pfY',
 })
-.then(function (result) {
-  var str = 'var brothers = ' + JSON.stringify(result, undefined, 2) + ';\n';
-  fs.writeFileSync('relations.js', str);
-})
-.catch(function (err) {
-  console.error('Something went wrong');
-  console.log(err.message);
-  console.log(err.stack);
-  process.exit(1);
-});
+  .then(function (result) {
+    var str = 'var brothers = ' + JSON.stringify(result, undefined, 2) + ';\n';
+    fs.writeFileSync('relations.js', str);
+  })
+  .catch(function (err) {
+    console.error('Something went wrong');
+    console.log(err.message);
+    console.log(err.stack);
+    process.exit(1);
+  });
 
