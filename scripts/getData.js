13adbf764bb0565a029a1ddb5d3a007eb06360f2
Nate Fischer <ntfschr@google.com>
2021-11-11 17:12:55 -0800

    refactor: switch to new google sheets library
    Google Drive visibility got locked down recently so I needed to switch
    to a new google sheets library. This is mostly straightforward, with
    some caveats:
    
    * The new library needs an API key
    * The library handles property names and values a bit differently, so I
      added some adapter code to map this onto the old scheme
    * Key-value pairs serialize in a different order now (not a big issue,
      but this will be apparent when I update the list of brothers in a
      future commit)
    
    Follow https://github.com/revolunet/spreadsheet-to-json to create an API
    key.

--- scripts/getData.js
+++ scripts/getData.js
@@ -1,13 +1,55 @@
-var gsjson = require('google-spreadsheet-to-json');
+var extractSheets = require('spreadsheet-to-json').extractSheets;
+
 var fs = require('fs');
 
-gsjson({
-  spreadsheetId: '1h6dVJKtETWX3Kr9PT6EaLu0gGavdi8Gnj4IlX155pfY',
+var apiKey;
+try {
+  apiKey = fs.readFileSync('local-api-key.txt', 'utf-8').trimRight('\n');
+} catch (e) {
+  var url = 'https://github.com/revolunet/spreadsheet-to-json';
+  console.error('You need an API key to run this. Please see ' + url +
+      ' and store this in local-api-key.txt in the project root');
+  process.exit(1);
+}
+
+extractSheets({
+  spreadsheetKey: '1h6dVJKtETWX3Kr9PT6EaLu0gGavdi8Gnj4IlX155pfY',
+  credentials: apiKey,
+  sheetsToExtract: ['Sheet1'],
+},
+function (err, result) {
+  if (err) {
+    console.error('Something went wrong');
+    console.log(err.message);
+    console.log(err.stack);
+    process.exit(1);
+  }
+
+  // We only need the data from Sheet1.
+  result = result.Sheet1;
+  result = result.map(function (bro) {
+    if (bro.familyStarted) {
+      bro.familystarted = bro.familyStarted;
+      delete bro.familyStarted;
+    }
+    // Un-stringify booleans.
+    Object.keys(bro).forEach(function (key) {
+      if (bro[key] === 'TRUE') {
+        bro[key] = true;
+      }
+    });
+    // Remove empty fields as a storage and readability optimization.
+    Object.keys(bro).forEach(function (key) {
+      if (bro[key] === null || bro[key] === undefined) {
+        delete bro[key];
+      }
+    });
+    return bro;
+  });
+
+  var str = 'var brothers = ' + JSON.stringify(result, undefined, 2) + ';\n';
+  fs.writeFileSync('relations.js', str);
 })
-  .then(function (result) {
-    var str = 'var brothers = ' + JSON.stringify(result, undefined, 2) + ';\n';
-    fs.writeFileSync('relations.js', str);
-  })
   .catch(function (err) {
     console.error('Something went wrong');
     console.log(err.message);
